3af9baff84ca543acc6ab2236e095095f2dca21a
ifaz5 <mutasim.ifaz@gmail.com>
2023-02-11 09:38:54 +0600

    railway distracted

--- src/Component/Wumpus.js
+++ src/Component/Wumpus.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-// import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, useParams } from "react-router-dom";
-// import auth from "../firebase.init";
+import auth from "../firebase.init";
 
 const Wumpus = () => {
   const [wumpus, setWumpus] = useState();
   const [loading, setLoading] = useState(true);
-  // const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const { id } = useParams();
   useEffect(() => {
     fetch(
